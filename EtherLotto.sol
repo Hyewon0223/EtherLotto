51cb7fd68ddd43e7543fa8a3268f814717b7e77c
Hyewon0223 <54919662+Hyewon0223@users.noreply.github.com>
2021-05-18 15:14:25 +0900

    Update EtherLotto.sol

--- EtherLotto.sol
+++ EtherLotto.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 
 pragma solidity >= 0.4.11;
-contract test_crowdFunding {
+contract EtherLotto {
 	// 투자자 구조체
 	struct Player {
 		address payable addr;	// 투자자의 어드레스
@@ -17,10 +17,12 @@ contract test_crowdFunding {
 	uint public goalAmount;		// 목표액
 	uint public totalAmount;	// 총 투자액
     mapping (uint => Player) public players;	// 투자자 관리를 위한 매핑
+    uint public price = 30000000000000000000;
 	
 	// 당첨자 정보
-	address public winnerAddress;
-
+	address public winnerAddress[];
+    uint public winnerNum = 1;
+    uint public randNum;
 	
 	modifier onlyOwner () {
 		require(msg.sender == owner);
@@ -45,13 +47,17 @@ contract test_crowdFunding {
 	/// 투자 시에 호출되는 함수
 	function fund() payable public{
 		// 모금이 끝났다면 처리 중단
-		require(!ended);
-		
+// 		require(!ended);
+		require(msg.value >= 10 ether, "Minimum value is 10 ETH"); // 10이더 이상일 때만 송금
 		Player storage play = players[numInvestors++];
 		play.addr = msg.sender;
 		play.amount = msg.value;
-		play.nums = 1;
+		play.nums = 1; // JS에서 받아와야 함
 		totalAmount += play.amount;
+		
+// 		if (price <= totalAmount){
+// 		    pickWinner();
+// 		}
 	}
 	
 	/// 목표액 달성 여부 확인
@@ -85,15 +91,6 @@ contract test_crowdFunding {
 		}
 	}
 	
-	// 추첨
-	function lotteryRandom() public onlyOwner {
-	    // 타임스탬프 값 설정
-		// timestamp = block.timestamp; => ?
-		
-		// 추첨
-		
-	} 
-	
 	/// 컨트랙트를 소멸시키기 위한 함수
 	function kill() public onlyOwner {
 		selfdestruct(owner);
@@ -109,18 +106,22 @@ contract test_crowdFunding {
     }
 	
 	function pickWinner() public onlyOwner {
-        uint randNum = generateRandomNumber();	// 난수 생성
-        uint idx = randNum % numInvestors;	// 당첨자 index
-        
-        address payable winner;		// 당첨자 address
-        
-        for(uint i=0; i<numInvestors; i++){
-            if(1 == players[i].nums){
-                winner = players[i].addr;
+	    if (goalAmount <= totalAmount){
+            randNum = generateRandomNumber();	// 난수 생성
+            
+            address payable winner;		// 당첨자 address
+            
+            for(uint i=0; i<numInvestors; i++){
+                if(winnerNum == players[i].nums){
+                    winner = players[i].addr;
+                }
             }
-        }
-        winner.transfer(address(this).balance);
-        //players = new address players [](0);
+            winner.transfer(address(this).balance);
+            //players = new address players [](0);
+	    }
+	   // else{
+	   //     revert();
+	   // }
     }
 	
 }
