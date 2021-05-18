5df62d4939979d93c2fc4b2c3800918b8dbce021
Hyewon0223 <54919662+Hyewon0223@users.noreply.github.com>
2021-05-19 01:25:37 +0900

    Update EtherLotto.sol

--- EtherLotto.sol
+++ EtherLotto.sol
@@ -14,14 +14,14 @@ contract EtherLotto {
 	uint public deadline;		// 마감일 (UnixTime)
 	string public status;		// 모금활동 상태
 	bool public ended;			// 모금 종료여부
-	uint public goalAmount;		// 목표액
+	uint public goalAmount  = 30000000000000000000;		// 목표액
 	uint public totalAmount;	// 총 투자액
     mapping (uint => Player) public players;	// 투자자 관리를 위한 매핑
-    uint public price = 30000000000000000000;
-	
+
 	// 당첨자 정보
-	address public winnerAddress[];
-    uint public winnerNum = 1;
+	address payable[] public winnerAddress;
+	uint public numWinner;      // 당첨자 수
+    uint public winningNum = 1; // 당첨 번호
     uint public randNum;
 	
 	modifier onlyOwner () {
@@ -30,55 +30,75 @@ contract EtherLotto {
 	}
 	
 	/// 생성자
-	constructor(uint _duration, uint _goalAmount) {
+	constructor(uint _duration) {
 		owner = msg.sender;
 
 		// 마감일 설정 (Unixtime)
 		deadline = block.timestamp + _duration;
 
-		goalAmount = _goalAmount;
-		status = "Funding";
+		status = "Buying";
 		ended = false;
 
 		numInvestors = 0;
 		totalAmount = 0;
+		numWinner = 0;  //test
 	}
 	
 	/// 투자 시에 호출되는 함수
 	function fund() payable public{
 		// 모금이 끝났다면 처리 중단
-// 		require(!ended);
-		require(msg.value >= 10 ether, "Minimum value is 10 ETH"); // 10이더 이상일 때만 송금
+		require(!ended);
+		require(msg.value == 10 ether, "Just Pay 10 ETH"); // 10이더 이상일 때만 송금
 		Player storage play = players[numInvestors++];
 		play.addr = msg.sender;
 		play.amount = msg.value;
 		play.nums = 1; // JS에서 받아와야 함
 		totalAmount += play.amount;
 		
-// 		if (price <= totalAmount){
-// 		    pickWinner();
-// 		}
+	}
+	
+	/// 조회하기
+	function getMyNum (address InputAddress) public view returns(uint){
+	    for (uint i=0;i<numInvestors;i++){
+	        if (players[i].addr == InputAddress)
+	            return players[i].nums;
+	    }
 	}
 	
 	/// 목표액 달성 여부 확인
 	/// 그리고 모금 성공/실패 여부에 따라 송금
-	function checkGoalReached () payable public onlyOwner {		
+	function checkGoalReached () payable public onlyOwner {
 		// 모금이 끝났다면 처리 중단
 		require(!ended);
 		
 		// 마감이 지나지 않았다면 처리 중단
 		require(block.timestamp >= deadline);
 		
-		if(totalAmount >= goalAmount) {	// 모금 성공인 경우
-			status = "Campaign Succeeded";
-			ended = true;
+		if(goalAmount <= totalAmount) {	// 모금 성공인 경우
+			status = "Succeeded";
 			// 컨트랙트 소유자에게 컨트랙트에 있는 모든 이더를 송금
-			if(!owner.send(address(this).balance)) {
-			    revert();
-			}
+	        ended = true;
+            randNum = generateRandomNumber();	// 난수 생성
+            
+            for(uint i=0; i<numInvestors; i++){
+                if(winningNum == players[i].nums){
+                   winnerAddress.push(players[i].addr);
+                   numWinner++;
+                }
+            }
+            shareMoney();
+            // winner.transfer(address(this).balance); // winner가 1명일때 송금
+            //players = new address players [](0); // 초기화
+            
+    //         for (uint j=0;j<numWinner;j++) {
+				// if(!winnerAddress[j].send(10 ether)) {
+				// 	revert();
+			 //   }
+			    
+	   //     }
 		} else {	// 모금 실패인 경우
 			uint i = 0;
-			status = "Campaign Failed";
+			status = "Failed";
 			ended = true;
 			
 			// 각 투자자에게 투자금을 돌려줌
@@ -89,6 +109,14 @@ contract EtherLotto {
 				i++;
 			}
 		}
+	}
+	
+	function shareMoney() payable public {
+	    for (uint j=0;j<numWinner;j++) {
+    		if(!winnerAddress[j].send(totalAmount/numWinner)) {
+    			revert();
+    	    }
+	    }
 	}
 	
 	/// 컨트랙트를 소멸시키기 위한 함수
@@ -106,22 +134,7 @@ contract EtherLotto {
     }
 	
 	function pickWinner() public onlyOwner {
-	    if (goalAmount <= totalAmount){
-            randNum = generateRandomNumber();	// 난수 생성
-            
-            address payable winner;		// 당첨자 address
-            
-            for(uint i=0; i<numInvestors; i++){
-                if(winnerNum == players[i].nums){
-                    winner = players[i].addr;
-                }
-            }
-            winner.transfer(address(this).balance);
-            //players = new address players [](0);
-	    }
-	   // else{
-	   //     revert();
-	   // }
+
     }
 	
 }
