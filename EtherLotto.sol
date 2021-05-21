fed43669ec94a16a50e31fbe33bd7bca67d7e2b5
Hyewon0223 <ch2w2006@kookmin.ac.kr>
2021-05-21 14:47:00 +0900

    ehter_Lotto.sol

--- EtherLotto.sol
+++ EtherLotto.sol
@@ -5,30 +5,31 @@ contract EtherLotto {
 	// 투자자 구조체
 	struct Player {
 		address payable addr;	// 투자자의 어드레스
-		uint amount;	        // 투자액
 		uint nums;
 	}
-	
+
 	address payable public owner;		// 컨트랙트 소유자
-	uint public numInvestors;	// 투자자 수
+	uint public numPlayers;	// 투자자 수
 	uint public deadline;		// 마감일 (UnixTime)
 	string public status;		// 모금활동 상태
 	bool public ended;			// 모금 종료여부
 	uint public goalAmount  = 30000000000000000000;		// 목표액 = 30 ether
-	uint public totalAmount;	// 총 투자액
-    mapping (uint => Player) public players;	// 투자자 관리를 위한 매핑
+	uint public totalAmount = 0;	// 총 투자액
+	bool public alreadyState;   // 중복된 사용자 확인
+	mapping (uint => Player) public players;	// 투자자 관리를 위한 매핑
+	uint public round = 0;
 
 	// 당첨자 정보
 	address payable[] public winnerAddress;
 	uint public numWinner;      // 당첨자 수
-    uint public winningNum = 1; // 당첨 번호
-    uint public randNum;
-	
+	uint public winningNum; // 당첨 번호
+	uint public randNum;    // 난수
+
 	modifier onlyOwner () {
 		require(msg.sender == owner);
 		_;
 	}
-	
+
 	/// 생성자
 	constructor(uint _duration) {
 		owner = msg.sender;
@@ -36,89 +37,109 @@ contract EtherLotto {
 		// 마감일 설정 (Unixtime)
 		deadline = block.timestamp + _duration;
 
-		status = "Buying";
+		status = "Available for purchase Lotto";
 		ended = false;
 
-		numInvestors = 0;
+		numPlayers = 0;
 		totalAmount = 0;
-		numWinner = 0;  //test
+		numWinner = 0;
+		alreadyState = false;
+		winningNum = 0;
+		round++;
 	}
-	
+
 	/// 투자 시에 호출되는 함수
-	function fund() payable public{
+	function purchase(uint playerNum) payable public{
 		// 모금이 끝났다면 처리 중단
 		require(!ended);
 		require(msg.value == 10 ether, "Just Pay 10 ETH"); // 10이더 이상일 때만 송금
-		Player storage play = players[numInvestors++];
+
+		for (uint i=0;i<numPlayers;i++){
+			if (msg.sender == players[i].addr) {
+				alreadyState = true;
+			}
+		}
+
+		require(!alreadyState, "Already Exist");
+
+		Player storage play = players[numPlayers++];
 		play.addr = msg.sender;
-		play.amount = msg.value;
-		play.nums = 1; // JS에서 받아와야 함
-		totalAmount += play.amount;
-		
+		play.nums = playerNum; // JS에서 받아와야 함
+		totalAmount += msg.value;
+
 	}
-	
-	/// 조회하기
+
+	/// 주소를 통해 조회하기
 	function getMyNum (address InputAddress) public view returns(uint){
-	    for (uint i=0;i<numInvestors;i++){
-	        if (players[i].addr == InputAddress)
-	            return players[i].nums;
-	    }
+		for (uint i=0;i<numPlayers;i++){
+			if (players[i].addr == InputAddress)
+				return players[i].nums;
+		}
+		return 0;
 	}
-	
+
 	/// 목표액 달성 여부 확인
-	/// 그리고 모금 성공/실패 여부에 따라 송금
-	function checkGoalReached () payable public onlyOwner {
+	/// 그리고 목표 금액 성공/실패 여부에 따라 송금
+	function pickWinner () payable public onlyOwner {
 		// 모금이 끝났다면 처리 중단
 		require(!ended);
-		
+
 		// 마감이 지나지 않았다면 처리 중단
 		require(block.timestamp >= deadline);
-		
+
 		if(goalAmount <= totalAmount) {	// 모금 성공인 경우
-			status = "Succeeded";
+			status = "Complete Success";
 			// 컨트랙트 소유자에게 컨트랙트에 있는 모든 이더를 송금
-	        ended = true;
-            randNum = generateRandomNumber();	// 난수 생성
-            randNum = randNum % 20 + 1;
-            for(uint i=0; i<numInvestors; i++){
-                if(winningNum == players[i].nums){
-                   winnerAddress.push(players[i].addr);
-                   numWinner++;
-                }
-            }
-            
-            for (uint j=0;j<numWinner;j++) {
-        		if(!winnerAddress[j].send(totalAmount/numWinner)) {
-        			revert();
-        	    }
-	        }
-		} else {	// 모금 실패인 경우
-			uint i = 0;
-			status = "Failed";
 			ended = true;
-			
+			randNum = getRandomNumber();	// 난수 생성
+			winningNum = randNum % 20 + 1;
+			for(uint i=0; i<numPlayers; i++){
+				if(winningNum == players[i].nums){
+					winnerAddress.push(players[i].addr);
+					numWinner++;
+				}
+			}
+
+			if (numWinner == 0) {
+				if(!owner.send(address(this).balance)) {
+					revert();
+				}
+			}
+			else {
+				for (uint j=0;j<numWinner;j++) {
+					if(!winnerAddress[j].send(totalAmount/numWinner)) {
+						revert();
+					}
+				}
+			}
+		}
+
+		else {	// 모금 실패인 경우
+			status = "Insufficient Amount";
+			ended = true;
+
 			// 각 투자자에게 투자금을 돌려줌
-			for (uint i=0;i<numInvestors;i++){
-				if(!players[i].addr.send(players[i].amount)) {
+			for (uint i=0;i<numPlayers;i++){
+				if(!players[i].addr.send(10 ether)) {
 					revert();
 				}
 			}
 		}
 	}
-	
-	
+
+
 	/// 컨트랙트를 소멸시키기 위한 함수
 	function kill() public onlyOwner {
 		selfdestruct(owner);
 	}
-	
+
 	function getBalance() view public returns(uint){
-	    return address(this).balance;
+		return address(this).balance;
 	}
-	
+
 	// 당첨 숫자 뽑기
-	function generateRandomNumber() public view returns (uint) {
-        return uint(keccak256(abi.encodePacked(block.timestamp, block.difficulty, numInvestors)));
-    }
-	
+	function getRandomNumber() public view returns (uint) {
+		return uint(keccak256(abi.encodePacked(block.timestamp, block.difficulty, numPlayers)));
+	}
+
 }
